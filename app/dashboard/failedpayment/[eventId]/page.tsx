1ca0f38676943ff6914d88b6e61f2368831671be
RaynerdTech <raynerdtech@gmail.com>
2025-07-10 00:55:28 +0100

    some changes

--- app/dashboard/failedpayment/[eventId]/page.tsx
+++ app/dashboard/failedpayment/[eventId]/page.tsx
@@ -1,9 +1,5 @@
 import ClientEventPage from './ClientEventPage';
 
-export default async function EventPage({
-  params,
-}: {
-  params: { eventId: string };
-}) {
+export default async function EventPage({ params }: { params: { eventId: string } }) {
   return <ClientEventPage eventId={params.eventId} />;
 }
